71caa8a8dc93bde34660f452875408ee19917e2e
zachrab <zachrab@gmail.com>
2011-07-22 00:03:07 -0700

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,9 @@ class SessionsController < ApplicationController
       @title = "Sign In"
       render 'new'
     else
-      # Sign the user in and redirect to the user's show page.
-    end
+      sign_in user
+      redirect_to user
+    end   
   end
   
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,18 +1,21 @@
 require 'spec_helper'
 
 describe SessionsController do
+  
   render_views
 
   describe "GET 'new'" do
-    it "should be successful" do
-      get :new
-      response.should be_success
-    end
     
-    it  "should have the right title" do
-      get :new
-      response.should have_selector("title", :content => "Sign In" )
-    end  
+      it "should be successful" do
+        get :new
+        response.should be_success
+      end
+    
+      it  "should have the right title" do
+        get :new
+        response.should have_selector("title", :content => "Sign In" )
+      end  
+      
   end
   
   describe "POST 'create'" do 
@@ -22,22 +25,44 @@ describe SessionsController do
       before(:each) do 
         @attr = { :email => "", :password => "" }
       end
-      
+    
       it "should re-render the new page" do 
         post :create, :session => @attr 
         response.should render_template('new')
       end
-      
+    
       it "should have the right title" do 
         post :create, :session => @attr 
         response.should have_selector("title", :content => "Sign In")
       end
-     
+   
       it "should have an error message" do 
         post :create, :session => @attr 
         flash.now[:error].should =~ /invalid/i
       end 
+      
     end
+    
+    describe "success" do
+      
+      before(:each) do 
+        @user = Factory(:user) 
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+  
+      it "should sign the user in" do 
+        post :create, :session => @attr 
+        controller.current_user.should == @user 
+        controller.should be_signed_in
+      end
+  
+      it "should redirect to the user show page" do 
+        post :create, :session => @attr 
+        response.should redirect_to(user_path(@user))
+      end
+    
+   end
+   
   end
   
   
